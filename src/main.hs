ee01bf085041c8698904b15aa3a8723bdba8ac9e
Dino Morelli <dino@ui3.info>
2009-07-10 21:28:42 +0000

    Started structuring the code with ErrorT
    Moved path to the acpid socket into the config. Logging a lot more things now as well.

--- src/main.hs
+++ src/main.hs
@@ -2,13 +2,65 @@
 -- License: BSD3 (see LICENSE)
 -- Author: Dino Morelli <dino@ui3.info>
 
-import Control.Monad ( forever )
+{-# LANGUAGE FlexibleContexts #-}
+
+import Control.Monad.Error
+import Data.Map
+import Data.Maybe
+import Fez.Data.Conf
+import Prelude hiding ( lookup )
 import Network.Socket
+import System.Directory
+import System.Exit
 import System.IO
 import System.Log
 
 import Uacpid.Conf
 import Uacpid.Log ( initLogging, logM )
+
+
+-- Lookup a key in a map in Error monad
+lookupE :: (MonadError String m) =>
+   String -> Map String a -> m a
+lookupE k m = maybe
+   (throwError ("required key " ++ k ++ " not found in config"))
+   return $ lookup k m
+
+
+openAcpidSocket :: (MonadError String m, MonadIO m) =>
+   ConfMap -> m Handle
+openAcpidSocket conf = do
+   acpidSocketPath <- lookupE "acpidSocket" conf
+
+   exists <- liftIO $ doesFileExist acpidSocketPath
+   unless exists $
+      throwError $ "socket not present at " ++ acpidSocketPath
+
+   hdl <- liftIO $ do
+      -- Open the UNIX domain socket
+      s <- socket AF_UNIX Stream defaultProtocol
+      connect s $ SockAddrUnix acpidSocketPath
+
+      -- Turn it into an ordinary handle
+      h <- socketToHandle s ReadMode
+      hSetBuffering h LineBuffering
+
+      return h
+
+   return hdl
+
+
+-- Read lines from the socket and do something with them
+listenAcpi :: Handle -> IO ()
+listenAcpi hdl = forever $ do
+   line <- hGetLine hdl
+   logM INFO $ "received from acpid: " ++ line
+
+
+exitFail :: String -> IO ()
+exitFail errMsg = do
+   logM ERROR errMsg
+   exitWith $ ExitFailure 1
 
 
 main :: IO ()
@@ -18,18 +70,12 @@ main = do
 
    logM NOTICE "uacpid daemon started"
 
-   -- Open the UNIX domain socket
-   s <- socket AF_UNIX Stream defaultProtocol
-   connect s $ SockAddrUnix "/var/run/acpid.socket"
+   logM NOTICE "uacpid daemon started"
+   logM NOTICE $ "logging level " ++
+      (fromJust $ lookup "logPriority" conf)
 
-   -- Turn it into an ordinary handle
-   hdl <- socketToHandle s ReadMode
-   hSetBuffering hdl LineBuffering
-
-   -- Read lines from it
-   forever $ do
-      line <- hGetLine hdl
-      logM INFO $ "received from acpid: " ++ line
+   eHdl <- runErrorT $ openAcpidSocket conf
+   either exitFail listenAcpi eHdl
 
    -- FIXME This never gets called when we ctrl-c
    logM NOTICE "uacpid daemon stopped"
