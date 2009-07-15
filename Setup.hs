56a69ecff04e760cdc08f8a0315659eeacc9ebad
Dino Morelli <dino@ui3.info>
2009-07-15 15:05:11 +0000

    Fixed copyright notice on Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,8 +1,8 @@
 #! /usr/bin/env runhaskell
 
--- Copyright: 2009 Ren Hoek
+-- Copyright: 2009 Dino Morelli
 -- License: BSD3 (see LICENSE)
--- Author: Ren Hoek <ren@spumco.co.dk>
+-- Author: Dino Morelli <dino@ui3.info>
 
 import Distribution.Simple
 
