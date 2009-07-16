e8811ec169a0131bf5c6e7d6a3351fbdacd50689
Dino Morelli <dino@ui3.info>
2009-07-16 02:47:45 +0000

    Reorganized the source code a bit, just cosmetic

--- Setup.hs
+++ Setup.hs
@@ -13,39 +13,40 @@ import System.Exit
 import System.FilePath
 
 
+copyManPage _ _ _ localBuildInfo = do
+   -- Construct src and dest dirs for copying the man page
+   let manFile = "uacpid.1"
+   let srcPath = "resources" </> "man" </> manFile
+
+   {- There's a "right way" to get the absolute mandir FilePath 
+      from the Cabal API, but I can't figure it out yet.
+      Have to revisit someday.
+   -}
+   let prefixDir = fromPathTemplate $ prefix
+         $ installDirTemplates localBuildInfo
+   let destDir =
+         prefixDir </> "share" </> "man" </> "man1"
+
+   createDirectoryIfMissing True destDir
+
+   putStrLn $ "Installing man page in " ++ destDir
+
+   let destPath = destDir </> manFile
+
+   -- Copy it to the appropriate man directory, based on the
+   -- prefix
+   copyFile srcPath destPath
+
+   -- gzip the man page
+   gzipExitCode <- system $ "gzip -f " ++ destPath
+
+   unless (gzipExitCode == ExitSuccess) $
+      putStrLn "Copy of man page failed!"
+
+   return ()
+
+
 main = defaultMainWithHooks (simpleUserHooks 
    { postCopy = copyManPage
    , postInst = copyManPage
    } )
-   where
-      copyManPage _ _ _ localBuildInfo = do
-         -- Construct src and dest dirs for copying the man page
-         let manFile = "uacpid.1"
-         let srcPath = "resources" </> "man" </> manFile
-
-         {- There's a "right way" to get the absolute mandir FilePath 
-            from the Cabal API, but I can't figure it out yet.
-            Have to revisit someday.
-         -}
-         let prefixDir = fromPathTemplate $ prefix
-               $ installDirTemplates localBuildInfo
-         let destDir =
-               prefixDir </> "share" </> "man" </> "man1"
-
-         createDirectoryIfMissing True destDir
-
-         putStrLn $ "Installing man page in " ++ destDir
-
-         let destPath = destDir </> manFile
-
-         -- Copy it to the appropriate man directory, based on the
-         -- prefix
-         copyFile srcPath destPath
-
-         -- gzip the man page
-         gzipExitCode <- system $ "gzip -f " ++ destPath
-
-         unless (gzipExitCode == ExitSuccess) $
-            putStrLn "Copy of man page failed!"
-
-         return ()
