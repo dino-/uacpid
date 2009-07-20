ce82c75e9790be7cfda96a7576b567a46a0df4b5
Dino Morelli <dino@ui3.info>
2009-07-20 00:15:52 +0000

    Another attempt to fix the man page installation problem
    This was a problem with how the dest path for the man page was being constructed when cabal copy is used by the Arch package builder to construct the install file tree.

--- Setup.hs
+++ Setup.hs
@@ -7,25 +7,47 @@
 import Control.Monad
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
 import System.Cmd
 import System.Directory
 import System.Exit
 import System.FilePath
 
 
-copyManPage _ _ _ localBuildInfo = do
-   -- Construct src and dest dirs for copying the man page
+copyManDestDir :: CopyFlags -> LocalBuildInfo -> FilePath
+copyManDestDir copyFlags localBuildInfo = destDir
+   where
+      Flag (CopyTo copyPrefix) = copyDest copyFlags
+
+      instPrefix = fromPathTemplate $ prefix
+         $ installDirTemplates localBuildInfo
+
+      {- Can't use </> here because instPrefix is often absolute
+         and </> returns the second path if ALONE if it's absolute.
+         Safer to just have the extra / if that's how it goes.
+      -}
+      destDir = copyPrefix ++ "/" ++ instPrefix
+         </> "share" </> "man" </> "man1"
+
+
+instManDestDir :: LocalBuildInfo -> FilePath
+instManDestDir localBuildInfo = destDir
+   where
+      {- There's a "right way" to get the absolute mandir FilePath 
+         from the Cabal API, but I can't figure it out yet.
+         Have to revisit someday.
+      -}
+      instPrefix = fromPathTemplate $ prefix
+         $ installDirTemplates localBuildInfo
+
+      destDir = instPrefix </> "share" </> "man" </> "man1"
+
+
+copyManPage :: FilePath -> IO ()
+copyManPage destDir = do
+   -- Construct src dir for copying the man page
    let manFile = "uacpid.1"
    let srcPath = "resources" </> "man" </> manFile
-
-   {- There's a "right way" to get the absolute mandir FilePath 
-      from the Cabal API, but I can't figure it out yet.
-      Have to revisit someday.
-   -}
-   let prefixDir = fromPathTemplate $ prefix
-         $ installDirTemplates localBuildInfo
-   let destDir =
-         prefixDir </> "share" </> "man" </> "man1"
 
    createDirectoryIfMissing True destDir
 
@@ -33,8 +55,7 @@ copyManPage _ _ _ localBuildInfo = do
 
    let destPath = destDir </> manFile
 
-   -- Copy it to the appropriate man directory, based on the
-   -- prefix
+   -- Copy it to the appropriate man directory
    copyFile srcPath destPath
 
    -- gzip the man page
@@ -47,6 +68,11 @@ copyManPage _ _ _ localBuildInfo = do
 
 
 main = defaultMainWithHooks (simpleUserHooks 
-   { postCopy = copyManPage
-   , postInst = copyManPage
+   { postCopy = customPostCopy
+   , postInst = customPostInst
    } )
+   where
+      customPostCopy _ copyFlags _ localBuildInfo =
+         copyManPage (copyManDestDir copyFlags localBuildInfo)
+      customPostInst _ _ _ localBuildInfo =
+         copyManPage (instManDestDir localBuildInfo)
